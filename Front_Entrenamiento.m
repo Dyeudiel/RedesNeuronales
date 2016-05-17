f8c658f6c5f5e116b75b8cc55f7981714cad0c55
Said <smoya@ekoomedia.co>
2016-05-16 21:31:44 -0500

    btn entrenar

--- Front_Entrenamiento.m
+++ Front_Entrenamiento.m
@@ -29,6 +29,8 @@ if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgr
     set(hObject,'BackgroundColor','white');
 end
 
+function btnEntrenar_Callback(hObject, eventdata, h)
+
 function btnAnalizar_Callback(hObject, eventdata, h)
     value = []
     value(1) = get(h.A1,'Value')
