1964d4a57af66b7b7ae4337781419fc57d96e967
Said <smoya@ekoomedia.co>
2016-05-19 20:20:23 -0500

    Aprender fix pequeño

--- aprender.m
+++ aprender.m
@@ -1,7 +1,8 @@
 function [res] = aprender(patron, ascii)
     [filas, columnas] = size(load('entradas.txt'))
 
-    salidaDeseada = horzcat([ascii], fliplr(de2bi(2^(filas-1), columnas)))
+    %concatena el ascii con la salida deseada
+    salidaDeseada = horzcat(ascii, fliplr(de2bi(2^(filas-1), columnas)));
 
     dlmwrite('entradas.txt', patron, 'delimiter', '\t', '-append')
-    &dlmwrite('datos.txt', salidaDeseada, 'delimiter', '\t', '-append')
+    dlmwrite('datos.txt', salidaDeseada, 'delimiter', '\t', '-append')
